fa352505b4624c6667dfcd97607f04ef1ae606bb
talking_chicken <talkingchicken@users.noreply.github.com>
2021-04-29 18:20:33 -0700

    empty program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace bytepusheremu
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new BytePusherEmu())
+                game.Run();
+        }
+    }
+}
